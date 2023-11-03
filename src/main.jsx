e988097650db9e3ba816f855f0f925466e949e86
Nicholas Rios <rios.nicholas@protonmail.com>
2023-11-02 23:05:30 -0400

    Create react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from './components/NavBar.jsx'
+import CreatePlayer from './routes/CreatePlayer.jsx'
+import SquadInfo from './routes/SquadInfo.jsx'
+import UpdatePlayer from './routes/UpdatePlayer.jsx'
+import PlayerDetails from './routes/PlayerDetails.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<NavBar />}>
+        <Route index={true} path='/' element={<App />} />
+        <Route index={true} path='/create-player' element={<CreatePlayer />} />
+        <Route index={true} path='/squad-info' element={<SquadInfo />} />
+        <Route index={true} path='/update-player/:id' element={<UpdatePlayer />} />
+        <Route index={true} path='/player-details/:id' element={<PlayerDetails />} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
