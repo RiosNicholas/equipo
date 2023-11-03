be43b042560ea2615d42f8044feb63f34b7402b5
Nicholas Rios <rios.nicholas@protonmail.com>
2023-11-02 23:05:50 -0400

    Remove unneeded code moved to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import './app.css'
 import { Routes, Route, Navigate, Outlet } from 'react-router-dom'
 import NavBar from './components/NavBar'
+import Home from './routes/Home'
 import Header from './components/Header'
-import PlayerCreation from './routes/PlayerCreation'
+import PlayerCreation from './routes/CreatePlayer'
 import SquadInfo from './routes/SquadInfo'
 import PlayerDetails from './routes/PlayerDetails'
 import UpdatePlayer from './routes/UpdatePlayer'
@@ -12,14 +13,7 @@ function App() {
 
   return (
     <div>
-      {/* <NavBar /> */}
-      <Routes>
-        <Route index element={<Home/>}/>
-        <Route path='player-creation' element={<PlayerCreation/>}/>
-        <Route path='squad-info' element={<SquadInfo/>}/>
-        <Route path='player-details/:id' element={<PlayerDetails />}/>
-        <Route path='player-details/update-player/:id' element={<UpdatePlayer/>}/>
-      </Routes>
+      <NavBar />
       <Header />
 
     </div>
