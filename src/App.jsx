eb7ca662d0608da4ad5a3694eab58690d893c06b
Nicholas Rios <rios.nicholas@protonmail.com>
2023-11-02 22:31:18 -0400

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import './app.css'
 import { Routes, Route, Navigate, Outlet } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Header from './components/Header'
+import PlayerCreation from './routes/PlayerCreation'
+import SquadInfo from './routes/SquadInfo'
+import PlayerDetails from './routes/PlayerDetails'
+import UpdatePlayer from './routes/UpdatePlayer'
 
 function App() {
 
   return (
     <div>
       {/* <NavBar /> */}
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path='player-creation' element={<PlayerCreation/>}/>
+        <Route path='squad-info' element={<SquadInfo/>}/>
+        <Route path='player-details/:id' element={<PlayerDetails />}/>
+        <Route path='player-details/update-player/:id' element={<UpdatePlayer/>}/>
+      </Routes>
       <Header />
+
     </div>
   )
 }
