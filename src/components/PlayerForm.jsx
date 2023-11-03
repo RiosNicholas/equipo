512d8827ba20a76939cffb15b79cfb17ea7680d0
Nicholas Rios <rios.nicholas@protonmail.com>
2023-11-02 22:30:58 -0400

    Update form components

--- src/components/PlayerForm.jsx
+++ src/components/PlayerForm.jsx
@@ -1,8 +1,74 @@
-const PlayerForm = () => {
-    return (
-        <>
-        </>
-    );
-}
+import { useState } from "react";
+import { supabase } from "../client";
 
-export default PlayerForm;
+const PlayerForm = () => {
+    const positions = ['GK', 'LB', 'CB', 'RB', 'CDM', 'CM', 'CAM', 'LM', 'RM', 'LW', 'RW', 'ST'];
+    const [player, setPlayers] = useState({name: "", nationality: "", position: ""});
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        console.log("Name: ", name, " value: ", value);
+        setCrewmate( (prev) => {
+            return {
+                ...prev,
+                [name]:value,
+            }
+        })
+    }
+
+    const createPlayer = async (event) => {
+        event.preventDefault(); 
+        await supabase
+            .from('Equipo')
+            .insert({
+                name: player.name, 
+                nationality: player.nationality, 
+                position: player.position 
+            })
+            .select();
+
+            window.location = "/SquadInfo";   
+    }
+
+    return (
+        <div>
+            <form>
+                <div>
+                    <label for="name">Name</label> <br />
+                    <div className="card">
+                        <input type="text" id="name" name="name" value={player.name} onChange={handleChange}/>
+                    </div>
+                </div>
+                
+                <div>
+                    <label for="nationality">Age</label><br />
+                    <div className="card">
+                        <input type="text" id="nationality" name="nationality" value={player.nationality} onChange={handleChange}/>
+                    </div>
+                </div>
+                    
+  
+                <div>
+                    <label for="position">Position</label><br />
+                    <div className="card">
+                        {positions.map((position) => (
+                            <li key={position}>
+                                <input 
+                                    id="position"
+                                    value={position}
+                                    name="position"
+                                    type="radio"
+                                    onChange={handleChange}
+                                />
+                                {position}
+                            </li>
+                        ))}
+                    </div>
+                </div>
+            </form>
+            <button type="submit" onClick={createPlayer}>Create</button>
+        </div>
+    );
+  };
+  
+  export default PlayerForm;
--- src/routes/UpdatePlayer.jsx
+++ src/routes/UpdatePlayer.jsx
@@ -1,7 +1,11 @@
+import UpdatePlayerForm from "../components/UpdatePlayerForm";
+
 const UpdatePlayer = () => {
     return (
-        <>
-        </>
+        <div>
+            <h2>Update Player</h2>
+            <UpdatePlayerForm />
+        </div>
     ); 
 }
 
